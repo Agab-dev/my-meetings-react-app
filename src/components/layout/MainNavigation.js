9ae70e3380ab927ec33b73b9356a9c1f8da05336
Agab <gameon.ahamd@gmail.com>
2025-03-06 17:35:22 +0300

    style - added the styling for the MainNavigation component

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom";
+import styles from "./MainNavigation.module.css";
 
 function Navbar() {
   return (
     <header>
-      <div>React Meetups</div>
+      <h1 className={styles.logo}>React Meetups</h1>
       <nav>
-        <ul>
+        <ul className={styles.nav}>
           <li>
-            <Link to={"/"}>All Meetups</Link>
+            <Link to={"/"} className={styles.link}>
+              All Meetups
+            </Link>
           </li>
           <li>
-            <Link to={"/new-meetup"}>Add New Meetup</Link>
+            <Link to={"/new-meetup"} className={styles.link}>
+              Add New Meetup
+            </Link>
           </li>
           <li>
-            <Link to={"/favorites"}>My Favorites</Link>
+            <Link to={"/favorites"} className={styles.link}>
+              My Favorites
+            </Link>
           </li>
         </ul>
       </nav>
