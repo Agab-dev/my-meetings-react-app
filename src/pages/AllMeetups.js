67b70bf8d97127cd8ef00e9e0e488b768e368aeb
Agab <gameon.ahamd@gmail.com>
2025-03-08 16:00:38 +0300

    feat - added the fetching of the meetups from firebase

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -22,10 +23,40 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(function () {
+    async function getMeetups() {
+      const res = await fetch(
+        "https://react-getting-started-4fab8-default-rtdb.firebaseio.com/meetups.json"
+      );
+      const data = await res.json();
+      const meetups = [];
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key],
+        };
+        meetups.push(meetup);
+      }
+      setIsLoading(false);
+      setMeetups(meetups);
+    }
+    getMeetups();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA}/>
+      <MeetupList meetups={meetups} />
     </section>
   );
 }
